f73eb11afc40e1855d503dd9586dd8bc60bc67bf
Timothy M. Schaeffer <numeromancer@gmail.com>
2015-05-31 17:59:09 -0400

    INCOMPLETE: more changes.

--- bowling-game-kata.js
+++ bowling-game-kata.js
@@ -1,14 +1,33 @@
 
 var debug = require("debug")("kata:bowling");
 
+function add(x, y) { return x + y; }
+
 function Game() {
     this.currentFrame =  0;
     this.roll = function (pins) {
+        if (this.currentFrame > 9) {
+            throw new Error('Game over, man!');
+        }
+        var f = this.frames[this.currentFrame];
+        var total = f.reduce(add, 0) + pins;
+        f.push(pins);
+        if (total == 10 || f.length === 2) {
+            this.currentFrame += 1;
+        }
     };
      this.score = function () {
-         return 0;
+         var score = 0;
+         this.frames.forEach(function(f) {
+             score += f.reduce(add, 0);
+         });
+         return score;
      };
-     this.frames = []; // 10 frames
+     this.frames = function() {
+         var a=[]; var i=0;
+         while(i++<10){ a.push([]); };
+         return a;
+     }(); // 10 frames
      return this;
 }
 
--- test/test1.js
+++ test/test1.js
@@ -13,4 +13,19 @@ describe("Game", function() {
         assert(game.score);
         assert.equal(game.score(), 0);
     });
+    it("should return a new game with 10 frames", function() {
+        var game = new bowling.Game();
+        assert.equal(game.frames.length, 10);
+    });
+    it("score should be 5 after single roll of 5", function() {
+        var game = new bowling.Game();
+        game.roll(5);
+        assert.equal(game.score(), 5);
+    });
+    it("score should be 10 after two rolls of 5", function() {
+        var game = new bowling.Game();
+        game.roll(5);
+        game.roll(5);
+        assert.equal(game.score(), 10);
+    });
 });
